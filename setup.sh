1c5aaee1994ad459a878119ba4a4c94d2beaef42
Igor Támara <igor.tamara@gmail.com>
2026-04-18 12:26:44 -0500

    applying migrations

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ python3.14 -m venv .venv
 source .venv/bin/activate
 python3.14 -m pip install -r requirements.txt
 
-#python manage.py migrate
-#python manage.py collectstatic
+python3.14 manage.py migrate
+python3.14 manage.py collectstatic
